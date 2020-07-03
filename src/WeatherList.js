a2c31546dc96123e604aefa568a3b940223a6a36
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-07-03 16:02:16 +0200

    Maj

--- src/WeatherList.js
+++ src/WeatherList.js
@@ -7,13 +7,14 @@ import Weather from './Weather';
 class WeatherList extends Component {
     state = { 
         appkey: "14bcf82a4395129027b7b7a341b60cad",
-        name: "lens",
+        name: "",
         periods: [],
     }
 
     handleChangeCity = (e) => {
+        let ville = e.target.value
         this.setState({
-            name: e.target.value,
+            name: ville
         })
     }
 
@@ -25,7 +26,18 @@ class WeatherList extends Component {
     }
 
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     axios.get('https://api.openweathermap.org/data/2.5/forecast?q='+this.state.name+'&lang=fr&units=metric&appid='+this.state.appkey)
+    //     .then(res => {
+    //         console.log(res.data)
+    //         this.setState({
+    //             periods: res.data.list,
+    //         })
+    //     })
+    // }
+
+    render() { 
+
         axios.get('https://api.openweathermap.org/data/2.5/forecast?q='+this.state.name+'&lang=fr&units=metric&appid='+this.state.appkey)
         .then(res => {
             console.log(res.data)
@@ -33,9 +45,6 @@ class WeatherList extends Component {
                 periods: res.data.list,
             })
         })
-    }
-
-    render() { 
         
         let meteos = this.state.periods.slice(0,4).map(period => {
             return <Weather period={period}/>
